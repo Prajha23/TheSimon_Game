f9a727fa609d62d14284b1641c27a10ae09fecec
Prajha23 <PrachiJha112303@gmail.com>
2024-10-11 16:21:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,90 +1,96 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
-var userClick = [];
+var userClickPattern = [];
 
 var started = false;
 var level = 0;
+var score = 0;
 var highScore = 0;
 
-$(document).keypress(function () {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSeq();
-    started = true;
-  }
-});
-
-$(".btn").click(function () {
-  var userColor = $(this).attr("id");
-  userClick.push(userColor);
-
-  playSound(userColor);
-  animate(userColor);
-  checkAnswer(userClick.length - 1);
-});
-
-$("#start-btn").click(function () {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSeq();
-    started = true;
-  } else {
-    startOver();
-  }
-});
-
-function checkAnswer(currentLevel) {
-  if (gamePattern[currentLevel] === userClick[currentLevel]) {
-    if (userClick.length === gamePattern.length) {
-      setTimeout(function () {
-        nextSeq();
-      }, 1000);
+$(document).keypress(function() {
+    if (!started) {
+        startGame();
     }
-  } else {
-    playSound("wrong");
-    $("body").addClass("game-over");
-    setTimeout(function () {
-      $("body").removeClass("game-over");
-    }, 200);
-    $("#level-title").text("Game Over, Press any key to restart or click Start");
-    if (level > highScore) {
-      highScore = level;
-      $("#high-score").text(highScore);
+});
+
+$("#start-btn").click(function() {
+    if (!started) {
+        startGame();
     }
-    startOver();
-  }
+});
+
+$(".btn").click(function() {
+    var userChosenColor = $(this).attr("id");
+    userClickPattern.push(userChosenColor);
+    
+    playsound(userChosenColor);
+    animate(userChosenColor);
+    checkAnswer(userClickPattern.length - 1);
+});
+
+function startGame() {
+    userClickPattern = [];
+    level = 0;
+    started = true;
+    $("#level-title").text("Level " + level);
+    nextSequence();
 }
 
-function nextSeq() {
-  userClick = [];
-  level++;
+function nextSequence() {
+    userClickPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+    
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
 
-  $("#level-title").text("Level " + level);
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playsound(randomChosenColor);
+    updateScore();
+}
 
-  var randomNum = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNum];
-  gamePattern.push(randomChosenColor);
-  $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-  playSound(randomChosenColor);
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickPattern[currentLevel]) {
+        if (userClickPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playsound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key or Start to Restart!");
+        resetGame();
+    }
+}
+
+function playsound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
 }
 
 function animate(currentColor) {
-  $("#" + currentColor).addClass("pressed");
-  setTimeout(function () {
-    $("#" + currentColor).removeClass("pressed");
-  }, 100);
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
 }
 
-function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
-  audio.play();
+function updateScore() {
+    score++;
+    $("#score").text(score);
+    if (score > highScore) {
+        highScore = score;
+        $("#high-score").text(highScore);
+    }
 }
 
-function startOver() {
-  level = 0;
-  gamePattern = [];
-  started = false;
-  $("#score").text(0);
+function resetGame() {
+    score = 0;
+    gamePattern = [];
+    started = false;
 }
-
-$("#score").text(level);
